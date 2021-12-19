6a4cf4c3740a4481f80145399efca0047efc057a
wuge <hswddan@qq.com>
2021-12-19 12:51:47 +0800

    tj

---
+++ dist/js/tj.js
@@ -0,0 +1,21 @@
+//百度统计代码
+var _hmt = _hmt || [];
+(function() {
+var hm = document.createElement("script");
+hm.src = "https://hm.baidu.com/hm.js?f3dfc3c319fc7568bebcb93ba5e873b5";
+var s = document.getElementsByTagName("script")[0]; 
+s.parentNode.insertBefore(hm, s);
+})();
+// 百度主动推送
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https'){
+bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+}
+else{
+bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+}
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
