1b2492fcc184d8e59936c7aa5a225240e73c8a50
wuge <hswddan@qq.com>
2022-04-30 08:41:45 +0800

    focussend

---
+++ dist/js/focussend.js
@@ -0,0 +1,9 @@
+(function() {
+    window.fs_config={
+        client_key:"0ef04caa-b925-fccb-1ebc-a7dbdc68c468"
+    };
+    var script = document.createElement("script");
+    script.src = "https://staticma.focussend.com/javascript/marketingchannel/website/focus_tracker.js?0ef04caa-b925-fccb-1ebc-a7dbdc68c468";
+    var document_script = document.getElementsByTagName("script")[0];
+    document_script.parentNode.insertBefore(script,document_script);
+})();
